91cffaf4ff74ebf6bb9bb6493730c4b1aac33828
xiedi <xie.di@jyall.com>
2016-10-13 21:24:28 +0800

    练习简单的流程控制

---
+++ dayone/logining.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Leon xie
+
+
+username = "xiedi"
+password = "123"
+
+user = input("用户名：")
+passwd = input("密码：")
+
+if user == username and passwd == password:
+    print("welcome to login.....")
+else:
+    print("登录失败")
+
