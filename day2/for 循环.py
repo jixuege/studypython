4f600d6ff4a0e9aefef32d830baa9b0bbae4e95d
xiedi <xie.di@jyall.com>
2016-10-16 10:50:21 +0800

    for 循环

---
+++ day2/for 循环.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Leon xie
+age=20
+#注意for下面的缩进问题
+for i in range(3):
+
+    gues_age = int(input("age:"))  # str --> int
+
+    if gues_age == age:
+        print("right!!")
+    elif  gues_age > age:
+        print("try smaller.....")
+    else:
+        print("try bigger...")
